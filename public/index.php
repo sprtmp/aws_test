c9d5830018837dc06cc8081ea7604170553a1713
сука <supertempmail0@gmail.com>
2025-06-05 16:27:50 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -13,5 +13,5 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../../aws_test/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../../bootstrap/app.php')
+(require_once __DIR__.'/../../aws_test/bootstrap/app.php')
     ->handleRequest(Request::capture());
